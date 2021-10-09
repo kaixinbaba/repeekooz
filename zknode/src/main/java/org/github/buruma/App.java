98a4a7959295d23896ab74e2389b5ae1f7990307
junjie.xun <junjie.xun@weimob.com>
2021-10-10 00:03:02 +0800

    增加 curator 依赖可以启动测试服务

---
+++ zknode/src/main/java/org/github/buruma/App.java
@@ -0,0 +1,21 @@
+package org.github.buruma;
+
+import org.apache.curator.test.TestingServer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        TestingServer testingServer = null;
+        try {
+            testingServer = new TestingServer(2181, false);
+            testingServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (testingServer != null) {
+                testingServer.close();
+            }
+        }
+    }
+}
